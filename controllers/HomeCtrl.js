e3e89dfd635f83251e75d5c54aeb8976a37fdabb
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-08 21:16:07 -0500

    modularized the first controll

---
+++ controllers/HomeCtrl.js
@@ -0,0 +1,18 @@
+'use strict'
+
+app.controller('HomeCtrl', function($scope, NgMap, WeatherFactory) {
+        $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIjfTBFP_wQKmOzUG8baijPFRcKCqUQ7w"
+         
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position){  
+                    let latitude = position.coords.latitude;
+                    let longitude = position.coords.longitude;
+                    WeatherFactory.getWeather(`http://api.wunderground.com/api/9a5695c0cb2e4520/geolookup/conditions/q/${latitude},${longitude}.json`)
+                        .then((results) => {
+                            console.log(results)
+                            $scope.hey = results.current_observation.display_location.full;
+                            $scope.fahr = results.current_observation.feelslike_f;
+                    })
+            });
+        };   
+})
--- main.js
+++ main.js
@@ -8,38 +8,19 @@ const app = angular.module('my-town', ['ngRoute', 'ngMap', ])
                     templateUrl: 'partials/home.html'
                 })
     )
-    .controller('HomeCtrl', function($scope, NgMap, WeatherFactory) {
-        $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIjfTBFP_wQKmOzUG8baijPFRcKCqUQ7w"
+    // .controller('HomeCtrl', function($scope, NgMap, WeatherFactory) {
+    //     $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIjfTBFP_wQKmOzUG8baijPFRcKCqUQ7w"
          
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position){  
-                let latitude = position.coords.latitude;
-                let longitude = position.coords.longitude;
-                WeatherFactory.getWeather(`http://api.wunderground.com/api/9a5695c0cb2e4520/geolookup/conditions/q/${latitude},${longitude}.json`)
-                .then((results) => {
-                    console.log(results)
-                    $scope.hey = results.current_observation.display_location.full;
-                    $scope.fahr = results.current_observation.feelslike_f;
-                })
-                });
-            };   
-    })
-    // .factory('WeatherFactory', function($q, $http){
-    //     const getWeather = (searchString) => { 
-    //         let latitude = '';
-    //         let longitude = '';
-    //         return $q(function(resolve, reject){
-    //             $http({
-    //                 method: 'GET',
-    //                 url: searchString
+    //         if (navigator.geolocation) {
+    //             navigator.geolocation.getCurrentPosition(function(position){  
+    //             let latitude = position.coords.latitude;
+    //             let longitude = position.coords.longitude;
+    //             WeatherFactory.getWeather(`http://api.wunderground.com/api/9a5695c0cb2e4520/geolookup/conditions/q/${latitude},${longitude}.json`)
+    //             .then((results) => {
+    //                 console.log(results)
+    //                 $scope.hey = results.current_observation.display_location.full;
+    //                 $scope.fahr = results.current_observation.feelslike_f;
     //             })
-    //             .success(function(results){
-    //                 resolve(results)
-    //             })
-    //             .catch(function(error){
-	// 		        reject(error);
-	// 	        });
-    //         })
-    //     }
-    //     return {getWeather}
+    //             });
+    //         };   
     // })
