215cd74279a94b04558b8443811ab1ea244aa46c
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-08 19:53:35 -0500

    added a hard coded weather underground api call and display the json results on the page

--- main.js
+++ main.js
@@ -12,11 +12,33 @@ angular
     .controller('HomeCtrl', function($scope, NgMap, Weather) {
         $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIjfTBFP_wQKmOzUG8baijPFRcKCqUQ7w"
 
-        $scope.hey = Weather
+        let searchString = 'http://api.wunderground.com/api/9a5695c0cb2e4520/conditions/q/CA/San_Francisco.json'
+
+    if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+             
+                Weather.getWeather(searchString)
+                    .then(function(results){
+                    $scope.hey = results
+                })
+            });
+        };        
     })
-    .factory('Weather', function($http){
-
-        var hey = "hey"
-
-        return hey
+    .factory('Weather', function($q, $http){
+        const getWeather = (searchString) => { 
+            return $q(function(resolve, reject){
+                $http({
+                    method: 'GET',
+                    url: searchString
+                })
+                .success(function(results){
+                    resolve(results)
+                    // console.log(results)
+                })
+                .catch(function(error){
+			        reject(error);
+		        });
+            })
+        }
+        return {getWeather}
     })
